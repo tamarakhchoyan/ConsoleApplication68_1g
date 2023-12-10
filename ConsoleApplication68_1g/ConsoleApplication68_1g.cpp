67f69e5aff213121211f40887492f9ef283d8cd6
User <User@DESKTOP-9TGTEUB>
2023-12-10 15:19:18 +0400

    Add project files.

---
+++ ConsoleApplication68_1g/ConsoleApplication68_1g.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+	const int n = 7;
+	int i, a[n], sum = 0;
+	for (i = 0; i < n; ++i)
+		std::cin >> a[i];
+	for(i=0;i<n;++i)
+		if(a[i]>5 && a[i]%2==0)
+		{
+			sum += a[i];
+			std::cout <<sum << std::endl;
+		}
+	return 0;
+}
